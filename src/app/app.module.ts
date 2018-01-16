d76fcd6133521969b1da9ab53e5046b0bfd24bb1
Alessandro <a.violini@e-xtrategy.net>
2018-01-16 14:26:13 +0100

    setup angular route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
+import { HomepageComponent } from './pages/homepage/homepage.component';
 
 import { AppComponent } from './app.component';
 
+const routes: Routes = [{
+    path: 'homepage',
+    component: HomepageComponent,
+    pathMatch: 'full'
+  }, {
+    path: '',
+    redirectTo: 'homepage',
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomepageComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent]
