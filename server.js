8dfe52732c812480e1ea1002bc8afdf69c7a3470
saverio <s.tosi@extrategy.net>
2018-03-13 13:49:39 +0100

    add socket-io

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 // Heroku server.js file
 
+var os = require('os');
 var express = require("express");
 var bodyParser = require("body-parser");
+var socketIO = require('socket.io');
+var cors = require('cors');
 
 var app = express();
+app.use(cors());
+
 app.use(bodyParser.json());
 
 // Create link to Angular build directory
@@ -11,9 +16,52 @@ var distDir = __dirname + "/dist/";
 app.use(express.static(distDir));
 
 // Initialize the app.
-var server = app.listen(process.env.PORT || 8080, function () {
+var server = app.listen(process.env.PORT || 8000, function () {
   var port = server.address().port;
   console.log("App now running on port", port);
 });
 
-// CONTACTS API ROUTES BELOW
+var io = socketIO(server);
+io.sockets.on('connection', function(socket) {
+
+  socket.on('message', function(message) {
+    console.log('Client said: ', message);
+    // for a real app, would be room-only (not broadcast)
+    socket.broadcast.emit('message', message);
+  });
+
+  socket.on('create or join', function(room) {
+    console.log('Received request to create or join room ' + room);
+
+    var clientsInRoom = io.sockets.adapter.rooms[room];
+    var numClients = clientsInRoom ? Object.keys(clientsInRoom.sockets).length : 0;
+    console.log('Room ' + room + ' now has ' + numClients + ' client(s)');
+
+    if (numClients === 0) {
+      socket.join(room);
+      console.log('Client ID ' + socket.id + ' created room ' + room);
+      socket.emit('created', room, socket.id);
+
+    } else if (numClients === 1) {
+      console.log('Client ID ' + socket.id + ' joined room ' + room);
+      io.sockets.in(room).emit('join', room);
+      socket.join(room);
+      socket.emit('joined', room, socket.id);
+      io.sockets.in(room).emit('ready');
+    } else { // max two clients
+      socket.emit('full', room);
+    }
+  });
+
+  socket.on('ipaddr', function() {
+    var ifaces = os.networkInterfaces();
+    for (var dev in ifaces) {
+      ifaces[dev].forEach(function(details) {
+        if (details.family === 'IPv4' && details.address !== '127.0.0.1') {
+          socket.emit('ipaddr', details.address);
+        }
+      });
+    }
+  });
+
+});
